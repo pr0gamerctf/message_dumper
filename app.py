af6696e35810f3151ef0a8217a1b007f6d49313a
pr0gamerctf <133567856+pr0gamerctf@users.noreply.github.com>
2023-05-14 17:04:29 +0800

    Create app.py
    Created the skeleton to dump messages from slack channel

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+
+try:
+    # Call the conversations.history method using the WebClient
+    # conversations.history returns the first 100 messages by default
+    # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
+    result = client.conversations_history(channel=channel_id)
+
+    # Print results
+    for message in result["messages"]:
+        print(message)
+
+except SlackApiError as e:
+    print("Error:", e)
