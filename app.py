f6c70b62ab6984469c6c33d5d0db5bf31b6125b0
pr0gamerctf <133567856+pr0gamerctf@users.noreply.github.com>
2023-05-14 17:10:47 +0800

    Update app.py
    Removed the comments of the code.

--- app.py
+++ app.py
@@ -2,16 +2,10 @@ import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-# Set the SLACK_BOT_TOKEN environment variable to your bot token
 client = WebClient(token="XXXXXXXXX-XXXXXXXXXXXX-XXXXXXXXXXX")
-
-# Set the channel ID to the channel you want to dump messages from
 channel_id = "1231242"
 
 try:
-    # Call the conversations.history method using the WebClient
-    # conversations.history returns the first 100 messages by default
-    # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
     result = client.conversations_history(channel=channel_id)
 
     # Print results
